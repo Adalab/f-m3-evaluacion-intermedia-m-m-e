a140d6329196cc3d1627649eb71988f6cc4051c6
Megan Edwards <megmyfedwards@gmail.com>
2019-05-13 10:12:17 +0200

    Add docs folder

---
+++ docs/precache-manifest.cd27ce707ba99b4f2226442cf98969ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec381b1725f2dc4a5201182186743172",
+    "url": "./index.html"
+  },
+  {
+    "revision": "15739e1f863ee99825cb",
+    "url": "./static/css/main.99698b94.chunk.css"
+  },
+  {
+    "revision": "2790c44532533aa1f119",
+    "url": "./static/js/2.2668f2aa.chunk.js"
+  },
+  {
+    "revision": "15739e1f863ee99825cb",
+    "url": "./static/js/main.eade049a.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
